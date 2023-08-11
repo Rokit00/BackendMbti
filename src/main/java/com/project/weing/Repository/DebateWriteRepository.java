a3ad6570caa8e440888b860e5db8d54303ed9eb7
solllz <vitaoback@gmail.com>
2023-08-12 01:41:33 +0900

    Revert "ver_0"
    This reverts commit 49307146869a7b4f104126eb43267d2af434b922.

--- src/main/java/com/project/weing/Repository/DebateWriteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.weing.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.weing.Entity.DebateWrite;
-
-public interface DebateWriteRepository extends JpaRepository<DebateWrite, Long> {
-}
--- src/main/java/com/project/weing/Repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.weing.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.weing.Entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
